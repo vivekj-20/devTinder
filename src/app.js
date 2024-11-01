e98902ebdf151fe3ce843cbd46cc8fea54e18071
vivek <vivekjena.20@gmail.com>
2024-11-01 16:02:45 +0530

    push

--- src/app.js
+++ src/app.js
@@ -1,17 +1,37 @@
 const express = require("express");
+const { userInfo } = require("os");
 
 const app = express();
 
-app.use("/myPage",(req,res)=>{
-    res.send("Hello Everyone , Welcome to Namaste NodeJS");
+app.get("/user/:userId/:name",(req,res,next)=>{
+    res.send({firstName:"vivek",lastName:"jena",userInfo:req.params.userId,name:req.params.name});
+    next();
+},(req,res,next)=>{
+    //res.send("welcome home 1");
+    next();
+},(req,res,next)=>{
+    //res.send("welcome home 2");
+    next();
+},
+(req,res)=>{
+    res.send("welcome home 3");
+    //next();
 })
 
-app.use("/home",(req,res)=>{
+app.post("/ho*m+e",(req,res)=>{
     res.send("welcome home");
 })
 
-app.use("/test",(req,res)=>{
+app.use("/tes?t",(req,res)=>{
     res.send("Test my knowledge here");
+})
+
+app.use(/.*none$/,(req,res)=>{
+    res.send("Test my knowledge here gojo");
+})
+
+app.use("/",(req,res)=>{
+    res.send("hellooooooo");
 })
 
 app.listen(7777,()=>{
