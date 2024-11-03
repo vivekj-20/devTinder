e3a7932b40dae2eb2d0860a47085d7a2fedc5889
vivek <vivekjena.20@gmail.com>
2024-11-03 12:14:28 +0530

    all_CRUD_operation

--- src/app.js
+++ src/app.js
@@ -4,15 +4,10 @@ const user = require("./model/user");
 
 const app = express();
 
+app.use(express.json());
 
 app.post("/signup",async (req,res)=>{
-    const dummy = new user({
-        firstName:"vivek",
-        lastName:"jena",
-        emailId:"vivek@jena.com",
-        age:"24",
-        gender:"M",
-    })
+    const dummy = new user(req.body)
     try{
        await dummy.save();
        res.send({result:"successfully added data of new user in DB !!!"});
@@ -22,6 +17,48 @@ app.post("/signup",async (req,res)=>{
     }
 })
 
+app.get("/user",async (req,res)=>{
+    try{
+       const users = await user.find({emailId:req.body.emailId}); 
+       res.send(users);
+    }
+    catch(err){
+       res.status(404).send({result:"data not found" + err.message});
+    }
+})
+
+app.delete("/user",async (req,res)=>{
+    const Id = req.body.userId;
+    try{
+        const users = await user.findByIdAndDelete(Id);
+        res.send({result:"successfully deleted data from DB !!!"});
+    }
+    catch(err){
+        res.status(404).send({result:"data not found" + err.message});
+    }
+})
+
+app.patch("/user",async (req,res)=>{
+    const Id = req.body.userId;
+    const data = req.body;
+    try{
+        const users = await user.findByIdAndUpdate(Id,data);
+        res.send({result:"successfully updated data in the DB !!!"});
+    }
+    catch(err){
+        res.status(404).send({result:"data not found" + err.message});
+    }
+})
+
+app.get("/feed",async (req,res)=>{
+    try{
+        const users = await user.find({}); 
+        res.send(users);
+    }
+    catch(err){
+        res.status(404).send({result:"data not found" + err.message});
+    }
+})
 connectDB().then(()=>{
     console.log("DB connected successfully !!!")
     app.listen(7777,()=>{
