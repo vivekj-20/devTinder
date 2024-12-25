20cf4bff200f0ddd5d8ca877f0c1d0d5eeab8dac
vivek <vivekjena.20@gmail.com>
2024-12-26 04:00:00 +0530

    feed and pagination

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const auth = require("../middleware/auth");
 const ConnectionRequest = require("../model/connectionRequest");
+const user = require("../model/user");
 
 const USER_SAFE_DATA = ["firstName","lastName","age","gender","skills","status"];
 
@@ -49,5 +50,46 @@ userRouter.get("/user/connections", auth, async (req, res) => {
      res.status(400).send({ message: err.message });
     }
 });
+
+userRouter.get("/feed", auth, async (req, res) => {
+
+    try{
+
+        const loggedInUser = req.data;
+
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
+        
+        const requests = await ConnectionRequest.find({
+            $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+          }).select("fromUserId  toUserId");
+
+        const hideUsersFromFeed = new Set(); // using a set data structure to avoid duplicate user id's values from DB.
+            
+        requests.forEach((req) => {
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        });
+        
+        const users = await user.find({
+            $and: [
+              { _id: { $nin: Array.from(hideUsersFromFeed) } },
+              { _id: { $ne: loggedInUser._id } },
+            ],
+          }).select(USER_SAFE_DATA)
+          .skip(skip)
+          .limit(limit);
+
+        res.json({
+            message: "Data fetched successfully",
+            data:users
+        });  
+    }catch(err){
+     res.status(400).send({ message: err.message });
+    }
+});
+
 
 module.exports = {userRouter};
