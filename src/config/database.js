ca87c3e8f519d72a7947087fc796dca82704ae31
Vivek Jena <vivekjena@Viveks-MacBook-Air.local>
2025-04-18 21:28:52 +0530

    code update for database

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async() =>{
-    await mongoose.connect("mongodb+srv://vivekjena20:khuA2CWVgzVZ2xuw@nodejs.w5xhu.mongodb.net/NodeJS");
+    await mongoose.connect("mongodb+srv://vivekjena20:ScU6FffSrzcR356W@nodejs.w5xhu.mongodb.net/NodeJS");
 }
 
 module.exports = connectDB;
